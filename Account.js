a5e4d8729e6d76211b810612e7af940598d41cc6
Renegadez518 <49694652+Renegadez518@users.noreply.github.com>
2023-03-06 22:14:58 +0530

    Update Account.js

---
+++ Account.js
@@ -0,0 +1,29 @@
+
+
+var users = [ ];
+function updatecsv()
+{
+    let converter = require('json-2-csv');
+    
+    let json2csvCallback = function (err, csv) 
+    {
+        if (err) throw err;
+        console.log(csv);
+    };
+    converter.json2csv(users, json2csvCallback);
+
+}
+function readCSVFile(file) {
+    let reader = new FileReader();
+    reader.onload = function() {
+      let lines = reader.result.split('\n');
+      lines.forEach(function(line) {
+        let values = line.split(',');
+        users.push(values);
+      });
+    };
+    reader.readAsText(file);
+}
+
+  
+  
