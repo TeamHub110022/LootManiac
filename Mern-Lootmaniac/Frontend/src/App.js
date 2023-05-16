8b6d5a7e3115ce8671a2e62a0cd8736265dc6146
Renegadez518 <49694652+Renegadez518@users.noreply.github.com>
2023-05-16 16:36:18 +0530

    frontend

--- Mern-Lootmaniac/Frontend/src/App.js
+++ Mern-Lootmaniac/Frontend/src/App.js
@@ -1,29 +1,66 @@
 import React, { useState } from 'react';
-import ExchangeForm from './components/ExchangeForm';
-import ExchangeRate from './components/ExchangeRate';
-import ExchangeResult from './components/ExchangeResult';
 
 function App() {
-  const App = () => {
-    const [exchangeRate, setExchangeRate] = useState(null);
-    const [convertedAmount, setConvertedAmount] = useState(null);
+  const [amount, setAmount] = useState('');
+  const [recipient, setRecipient] = useState('');
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
   };
+
+  const handleRecipientChange = (e) => {
+    setRecipient(e.target.value);
+  };
+
+  const handleTransfer = () => {
+    if (amount <= 0) {
+      console.log('Invalid amount');
+      return;
+    }
+  
+    if (recipient.trim() === '') {
+      console.log('Recipient cannot be empty');
+      return;
+    }
+  
+    fetch('/api/transfer', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount, recipient }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Transfer successful');
+        // Do something with the response data if needed
+      })
+      .catch((error) => {
+        console.log('Transfer failed:', error);
+      });
+    console.log(`Transferring ${amount} to ${recipient}`);
+  };
+
   return (
     <div>
-      <h2>Money Exchange App</h2>
-      <ExchangeForm handleExchange={handleExchange} />
-      {exchangeRate && <ExchangeRate rate={exchangeRate} />}
-      {convertedAmount && (
-        <ExchangeResult
-          amount={amount}
-          fromCurrency={fromCurrency}
-          toCurrency={toCurrency}
-          convertedAmount={convertedAmount}
-        />
-      )}
+      <h1>Money Transfer App</h1>
+      <form>
+        <label>
+          Amount:
+          <input type="number" value={amount} onChange={handleAmountChange} />
+        </label>
+        <br />
+        <label>
+          Recipient:
+          <input type="text" value={recipient} onChange={handleRecipientChange} />
+        </label>
+        <br />
+        <button type="button" onClick={handleTransfer}>
+          Transfer Money
+        </button>
+      </form>
     </div>
   );
-  
 }
 
 export default App;
